2fc02e9404ce8f8ff9a406a6502cc8a14c05a3dc
clmb00 <paolo.colombo2000@gmail.com>
2022-10-19 18:10:51 +0200

    [ADD] comments

--- js/script_paridispari.js
+++ js/script_paridispari.js
@@ -2,22 +2,27 @@ document.addEventListener('keypress', function(event){
 
   if (event.key != "Enter") return;
 
+  // Prendere l'elemnto dove scrivo il risultato e rimuovo eventuali classi di una precedente iterazione
   const outputBox = document.querySelector('p');
   outputBox.classList.remove('text-green-700', 'font-bold', 'text-red-700');
+  // Prendere il box dove scriverò il numero estratto
   const aiBox = document.getElementById('aiNumber');
+  // Prendere il numero inserito dall'utente e fare i controlli
   const numberUser = parseInt(document.querySelector('[type=text]').value);
   if (wrongInput(numberUser)) {
     alert('Inserisci un numero tra 1 e 5');
     return;
   }
-  //True se è pari, False se è dispari
+  //Prendere la scelta del utente sul pari o dispari
+  // True se è pari, False se è dispari
   const choiceUser = document.getElementById('pari').checked;
-
+  // Calcolare il numero casuale e scriverlo
   const randomNumber = getRandomNumber(1,5);
   aiBox.innerHTML = randomNumber;
 
   const somma = numberUser + randomNumber;
 
+  // Controllare chi ha vinto
   if (checkPariDispari(choiceUser,somma)) {
     outputBox.innerHTML = 'Hai vinto!, La somma è ' + somma;
     outputBox.classList.add('text-green-700', 'font-bold');
@@ -33,17 +38,20 @@ function getRandomNumber(min,max){
 }
 
 function checkPariDispari(isPari,somma){
+  // Se lutente ha scelto pari e la somma è pari return true
   if(isPari && !(somma%2)){
     return true;
-  } else if(!isPari && somma%2){
+  } else if(!isPari && somma%2){ //Oppure se lutente ha scelto dispari e la somma è dispari returna sempre true
     return true;
-  } else {
+  } else { //Altirmenti return false
     return false;
   }
 }
 
 function wrongInput(num){
+  // Se non è un numero ritorna true
   if (isNaN(num)) return true;
+  // Se non è compreso tra 1 e 5 ritorna true
   if (num<1 || num>5){
     return true;
   } else {
